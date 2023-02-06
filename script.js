ff810963dc86052cce17365a490ae746a9feb291
Manikuzzaman1990 <manikuzzaman1111@gmail.com>
2023-02-06 16:05:08 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,129 @@
+// 1. Type conversion
+// const inputNumber = "7500";
+// console.log(inputNumber + 10);
+
+// console.log(inputNumber);
+// console.log(Number(inputNumber), inputNumber);
+// console.log(typeof inputNumber);
+
+// const birthYear = 1990;
+// console.log(String(birthYear));
+
+// const firstName = "Marzia";
+// console.log(Number(firstName));
+
+// const inputNumber = "7500";
+// const firstName = "Marzia";
+// console.log(Boolean(inputNumber));
+// console.log(Boolean(firstName));
+
+// 2. JavaScript type Coercion
+// let bookNumber = 5;
+// console.log("I have " + bookNumber + " Stroy Book!");
+// console.log("20" - 5);
+// console.log("20" + 5);
+// console.log("20" * 5);
+// console.log("20" / 5);
+// console.log("20" - "5");
+
+// console.log(5 + 10 + 5 + "5");
+// console.log("100" - "50" + 10);
+// console.log("100" - "50" + "10");
+// console.log("15" - "5" - "4" - "3" + 5);
+// console.log("15" - "5" - "4" - "3" + "5");
+
+// 3. JavaScript Truthy and Falsy Value
+// 0
+// ""
+// null
+// undefined
+// NaN
+
+// console.log(0);
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean(1));
+// console.log(Boolean("Marzia"));
+// console.log(Boolean("1234"));
+
+// const job = 1;
+// if (job) {
+//   console.log("I have a Good Job!");
+// } else {
+//   console.log(" Looking for a Good Job!");
+// }
+
+// let salary;
+// if (salary) {
+//   console.log("My salary is set");
+// } else {
+//   console.log("My salary isn't set");
+// }
+
+// 4. Conditional Statements
+// let earning = 15000;
+// if (earning > 35000) {
+//   console.log("His earning is good");
+// } else if (earning > 20000) {
+//   console.log("His earning isn't bad");
+// } else if (earning > 10000) {
+//   console.log("Write Something");
+// } else {
+//   console.log("His earning isn't good enough!");
+// }
+
+// let today = "Friday";
+// if (today === "Saturday") {
+//   console.log("Today 9pm we have a main live class");
+// } else if (today === "Sunday") {
+//   console.log("Today 9pm we have a support class");
+// } else if (today === "Manday") {
+//   console.log("Today 9pm we have a main live class");
+// } else if (today === "Tuesday") {
+//   console.log("Today 9pm we have a support class");
+// } else if (today === "Wednesday") {
+//   console.log("Today 9pm we have a main live class");
+// } else {
+//   console.log("Today is our holiday");
+// }
+
+// 5. Nested Conditional Statements
+// const x = 10;
+// if (x >= 10) {
+//   if (x == 10) {
+//     console.log("X is equal 10!");
+//   } else {
+//     console.log("X is greter than 10!");
+//   }
+// } else {
+//   console.log("X is lower than 10!");
+// }
+
+// 6. Switch  Statements
+// let day = "Friday";
+
+// switch (day) {
+//   case "Saturday":
+//     console.log("Today 9pm we have a live main clss!");
+//     break;
+//   case "Sunday":
+//     console.log("Today 9pm we have a live support clss!");
+//     break;
+//   case "monday":
+//     console.log("Today 9pm we have a live main clss!");
+//     break;
+//   case "Tuesday":
+//     console.log("Today 9pm we have a live support clss!");
+//     break;
+//   case "Wednesday":
+//     console.log("Today 9pm we have a live main clss!");
+//     break;
+//   case "Thursday":
+//     console.log("Today 9pm we have a live support clss!");
+//     break;
+//   default:
+//     console.log(" Today is our holiday");
+// }
